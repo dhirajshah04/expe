343455a82e19a3800be26d78c5d67c17af4e945f
dhirajshah04 <dhiraj.shah04@gmail.com>
2017-09-08 14:16:01 +0545

    Extract email and phone number

---
+++ expe.py
@@ -0,0 +1,20 @@
+import re
+#import pyperclip
+
+#text = pyperclip.paste()
+
+#Reads the data to be extracted from list.txt file
+
+with open("list.txt", 'r') as f:
+		email = re.findall(r'[\w.-]+@[\w-]+[\.][\w]+', f.read())		
+		with open("emailid.txt", 'w+') as f:
+			for line in email:
+				f.write(line+"\n")
+				print(line)
+
+with open("list.txt", 'r') as f:
+	phone = re.findall(r'\+\d{3}\s?0?\d{6,10}', f.read())
+	with open("phone.txt", 'w+') as f:
+		for line in phone:
+			f.write(line+"\n")
+			print(line)
